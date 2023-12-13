add874167104061b573abaf89cb91a938c847452
riooastfu <rio.alaska123@gmail.com>
2023-12-13 17:22:17 +0700

    add invitation code

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,8 +2,9 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
-import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -11,6 +12,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const {onOpen} = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
   return (
@@ -21,6 +24,65 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           <ChevronDown className="h-5 w-5 ml-auto" />
         </button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+      >
+        {
+          isModerator && (
+            <DropdownMenuItem 
+            onClick={()=> onOpen("invite", {server: server})}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+              Invite pepople
+              <UserPlus className="h-4 w-4 ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+        {
+          isAdmin && (
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+              Server Setting
+              <Settings className="h-4 w-4 ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+        {
+          isAdmin && (
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+              Manage Members
+              <Users className="h-4 w-4 ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+        {
+          isModerator && (
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+              Create Channel
+              <PlusCircle className="h-4 w-4 ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+        {
+          isModerator && (
+            <DropdownMenuSeparator/>
+          )
+        }
+        {
+          isAdmin && (
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer text-rose-500">
+              Delete Server
+              <Trash className="h-4 w-4 ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+        {
+          !isAdmin && (
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer text-rose-500">
+              Leave Server
+              <LogOut className="h-4 w-4 ml-auto"/>
+            </DropdownMenuItem>
+          )
+        }
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
---
+++ hooks/use-origin.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export const useOrigin = () => {
+    const [mounted, setmounted] = useState(false);
+
+    useEffect(()=>{
+        setmounted(true)
+    },[]);
+
+    const origin = typeof window !== "undefined" && window.location.origin ? window.location.origin : "";
+
+    if(!mounted){
+        return "";
+    }
+
+    return origin
+
+}
